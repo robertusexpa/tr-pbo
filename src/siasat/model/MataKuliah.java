b6cc950c18963858b9989012024c6920257b4321
paiiiafterain <ekoprstyo1212@gmail.com>
2025-11-30 21:35:03 +0700

    Add files via upload

---
+++ src/siasat/model/MataKuliah.java
@@ -0,0 +1,53 @@
+package siasat.model;
+
+public class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+    private String semester; // optional
+
+    public MataKuliah() {}
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    @Override
+    public String toString() {
+        return kode + " - " + nama + " (" + sks + " SKS)";
+    }
+}
